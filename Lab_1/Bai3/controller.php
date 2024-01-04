fa549b5eaacc9ed5dcb8bc0d3d446cef5899f109
dang12e <ctytnhh5thanhvienvogiacu@gmail.com>
2024-01-04 17:30:28 +0700

    Lab1

--- Lab_1/Bai3/controller.php
+++ Lab_1/Bai3/controller.php
@@ -1,6 +1,5 @@
 <?php
 include 'model.php';
-echo 'PC07460 - Lab1 1.3 <br>';
 //controller
 if (isset($_POST['submit'])){
 $email = $_POST['email'];
--- Lab_1/Bai3/view.php
+++ Lab_1/Bai3/view.php
@@ -1,40 +1,40 @@
-
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Email and Password Validation</title>
-    <!-- CSS -->
-    <link rel="stylesheet" href="style.css" />
-    <!-- Boxicons CSS -->
-    <link href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css"rel="stylesheet"/>
-  </head>
-  <body>
-    <div class="container">
-      <header>Search</header>
-      <form method="post">
-        <?
-        if (!empty($user)){
-            echo 'Name: '.$user['name'];
-            echo '<br>';
-            echo 'Email: '.$user['email'];
-        }
-        ?>
-        <div class="field email-field">
-          <div class="input-field">
-            <input type="email" placeholder="Enter your email" class="email" name="email"/>
-          </div>
-          <span class="error email-error">
-            <i class="bx bx-error-circle error-icon"></i>
-            <p class="error-text">Please enter a valid email</p>
-          </span>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Email and Password Validation</title>
+  <!-- CSS -->
+  <link rel="stylesheet" href="style.css" />
+  <!-- Boxicons CSS -->
+  <link href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css" rel="stylesheet" />
+</head>
+
+<body>
+  <div class="container">
+    <?
+    echo '<header>PC07460 - Lab1 1.3 <br></header>';
+    ?>
+    <form method="post">
+      <?
+      if (!empty($user)) {
+        echo 'Name: ' . $user['name'];
+        echo '<br>';
+        echo 'Email: ' . $user['email'];
+      }
+      ?>
+      <div class="field email-field">
+        <div class="input-field">
+          <input type="email" placeholder="Enter your email" class="email" name="email" />
         </div>
-        <div class="input-field button">
+      </div>
+      <div class="input-field button">
         <button type="submit" name="submit">Gửi</button>
-        </div>
-      </form>
-    </div>
-  </body>
+      </div>
+    </form>
+  </div>
+</body>
+
 </html>
