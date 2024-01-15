7979d3502b76331746df5727e8235ab69bbac464
dang12e <ctytnhh5thanhvienvogiacu@gmail.com>
2024-01-15 15:02:00 +0700

    1

--- Asm/src/Model/UserFunction.php
+++ Asm/src/Model/UserFunction.php
@@ -1,12 +1,14 @@
 <?
+
 namespace Src\Model;
 
-class UserFunction{
+class UserFunction
+{
 
-public function checkAccount($Email, $password)
+    public function checkAccount($Email, $password)
     {
         $pass_hash = md5($password);
-        $db = new connect();
+        $Data = new Database();
         $select = "SELECT * FROM user WHERE email = '$Email' AND user_password = '$pass_hash' AND is_deleted = 2";
         $result = $db->pdo_query_one($select);
         if ($result != null)
@@ -16,9 +18,9 @@ public function checkAccount($Email, $password)
     }
 
     public function checkUser($Email, $password)
-    {   
+    {
         $pass_hash = md5($password);
-        $db = new connect();
+        $Data = new Database();
         $select = "SELECT * FROM user WHERE email = '$Email' AND user_password = '$pass_hash'";
         $result = $db->pdo_query_one($select);
         if ($result != null)
@@ -30,7 +32,7 @@ public function checkAccount($Email, $password)
     public function checkRole($Email, $password)
     {
         $pass_hash = md5($password);
-        $db = new connect();
+        $Data = new Database();
         $select = "SELECT user.role_id FROM user, `role` 
         WHERE user.role_id = role.role_id AND email = '$Email' AND user_password ='$pass_hash'";
         $result = $db->pdo_query_one($select);
@@ -39,7 +41,7 @@ public function checkAccount($Email, $password)
 
     function getInfoUserEmail($Email, $column)
     {
-        $db = new connect();
+        $Data = new Database();
         $sql = "SELECT * FROM user WHERE email ='$Email'";
         $result = $db->pdo_query($sql);
         foreach ($result as $row) {
@@ -49,11 +51,9 @@ public function checkAccount($Email, $password)
 
     public function userid($userAccount, $password)
     {
-        $db = new connect();
+        $Data = new Database();
         $select = "SELECT user_id FROM khachhang WHERE ho_ten='$userAccount' AND mat_khau='$password'";
         $result = $db->pdo_query_one($select);
         return $result;
     }
-
 }
-?>
